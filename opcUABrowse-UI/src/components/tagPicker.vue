10eadea6b28ff98014925f0a216a2ea9100d9a0c
Max Eichhorn <m.3ichhorn@gmail.com>
2022-06-14 21:44:01 +0200

    commit

--- opcUABrowse-UI/src/components/tagPicker.vue
+++ opcUABrowse-UI/src/components/tagPicker.vue
@@ -2,14 +2,26 @@
   <div>
     <h3>Datenpunkt Selektion</h3>
     <div>
+     <ul>
+        <li v-for="tag in setTags" :key="tag.nodeId">{{tag.nodeId}}{{tag.browseName.name}}
+          <ul>
+            
+          </ul>
+        </li>
+     </ul>
      
-      
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed:{
+    setTags(){
+      return this.$store.getters.tagGetter
+    }
+  }
+};
 </script>
 
 <style>
